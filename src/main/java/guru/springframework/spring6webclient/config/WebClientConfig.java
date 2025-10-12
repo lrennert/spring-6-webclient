8da358064c95765012c1218cbfc02d2fc3af8505
Lydia Rennert <lydia.rennert@gmail.com>
2025-10-12 11:44:30 +0200

    lre: Configure WebClient to use OAuth2 security

--- src/main/java/guru/springframework/spring6webclient/config/WebClientConfig.java
+++ src/main/java/guru/springframework/spring6webclient/config/WebClientConfig.java
@@ -3,19 +3,29 @@ package guru.springframework.spring6webclient.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig implements WebClientCustomizer {
 
     private final String rootUrl;
+    private final ReactiveOAuth2AuthorizedClientManager manager;
 
-    public WebClientConfig(@Value("${webclient.rooturl}") String rootUrl) {
+    public WebClientConfig(@Value("${webclient.rooturl}") String rootUrl,
+                           ReactiveOAuth2AuthorizedClientManager manager) {
         this.rootUrl = rootUrl;
+        this.manager = manager;
     }
 
     @Override
     public void customize(WebClient.Builder webClientBuilder) {
-        webClientBuilder.baseUrl(rootUrl);
+        ServerOAuth2AuthorizedClientExchangeFilterFunction oauth =
+                new ServerOAuth2AuthorizedClientExchangeFilterFunction(manager);
+
+        oauth.setDefaultClientRegistrationId("my-client");
+
+        webClientBuilder.filter(oauth).baseUrl(rootUrl);
     }
 }
